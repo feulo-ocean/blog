bdbbf2b53f7cf72e7fb47a2c2b40c49d40a6fad3
feulo-ocean <guilherme.feulo@oceanbrasil.com>
2022-07-26 23:05:47 +0000

    intervalo aula 3

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
-@app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello Word"
+posts = [
+    {
+        "title": "O meu Primeiro Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Feulo",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "O meu Segundo Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Danilo",
+        "created": datetime(2022,7,26)
+    },
+]
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
 
