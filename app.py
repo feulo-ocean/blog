f1c5041f33a6b8c8589c717915f4f692600fa0c2
feulo-ocean <guilherme.feulo@oceanbrasil.com>
2022-07-30 00:30:03 +0000

    teste

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ db.create_all()
 
 @app.route("/")
 def index():
-    posts = Post.query.all()
+    posts = Post.query.order_by(-Post.created).all()
     return render_template("index.html", posts=posts)
 
 @app.route('/register', methods=["GET","POST"])
